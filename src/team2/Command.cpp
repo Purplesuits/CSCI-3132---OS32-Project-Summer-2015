0de3bbeda1063d6d7db76f9ddeaaa04d921f27d9
Alexander Zinck <zinckalexander@gmail.com>
2015-07-23 15:10:02 -0300

    Added some more comments and error checking.

--- src/team2/Command.cpp
+++ src/team2/Command.cpp
@@ -8,9 +8,13 @@
 int CurrDir::execute() {
 	FileSystem &fileSystem = FileSystem::getInstance();
 	File * currDir = fileSystem.getCurrDir();
+	
+	//curr dir doesn't exist
 	if (currDir == nullptr) {
+		UI::println("Error. Current directory unknown.");
 		return EXIT_FAILURE;
 	}
+
 	std::string filename =  currDir->getFileName();
 	UI::println(filename);
 	return EXIT_SUCCESS;
@@ -25,7 +29,8 @@ int HelpMe::execute() {
 		UI::println(contents);
 		return EXIT_SUCCESS;
 	} else {
-		UI::println("File command-help.txt not found.");
+		//file not found
+		UI::println("Error. File command-help.txt not found.");
 		return EXIT_FAILURE;
 	}
 }
@@ -43,6 +48,7 @@ int FCreate::execute() {
 	std::vector<bool> permissions = std::vector<bool>(3, true);
 	bool worked = fileSystem.fcreate(filename, "stuff", permissions);
 
+	//fcreate failed
 	if (!worked) {
 		UI::println("Error. File creation failed.");
 		return EXIT_FAILURE;
@@ -53,9 +59,11 @@ int FCreate::execute() {
 
 int DCreate::execute() {
 	FileSystem &fileSystem = FileSystem::getInstance();
+
 	if (fileSystem.dcreate(dirname)) {
 		return EXIT_SUCCESS;
 	} else {
+		//dcreate failed
 		UI::println("Error. Failed to create directory.");
 		return EXIT_FAILURE;
 	}
@@ -65,7 +73,15 @@ int FRead::execute() {
 	FileSystem &fileSystem = FileSystem::getInstance();
 	File * file = fileSystem.fread(filename);
 	
+	//file doesn't exist
 	if (file == nullptr) {
+		UI::println("Error. File not found.");
+		return EXIT_FAILURE;
+	}
+
+	//user lacks read permissions
+	if (!file->getPermissions().at(0)) {
+		UI::println("Error. File lacks read permissions.");
 		return EXIT_FAILURE;
 	}
 		
@@ -78,7 +94,15 @@ int FWrite::execute() {
 	FileSystem &fileSystem = FileSystem::getInstance();
 	File * file = fileSystem.fLocate(filename);
 
+	//file doesn't exist
 	if (file == nullptr) {
+		UI::println("Error. File not found.");
+		return EXIT_FAILURE;
+	}
+
+	//file lacks write permissions
+	if (!file->getPermissions().at(1)) {
+		UI::println("Error. File lacks write permissions.");
 		return EXIT_FAILURE;
 	}
 
@@ -93,6 +117,7 @@ int ChPer::execute() {
 		UI::println("usage: chper <filename> <permissions>: permissions should be a three bit mask for read, write, execute permissions");
 	}
 
+	//assumes user enters a bitmask
 	std::vector<bool> p;
 	p.push_back(permissions.at(0) == '1');
 	p.push_back(permissions.at(1) == '1');
