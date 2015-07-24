c5f3bfdebe62d2fadb2a23063e0305e277e3340c
xiangling1994 <neolx199@gmail.com>
2015-07-24 08:56:01 -0300

    Update calculator.cpp

--- src/team4/calculator.cpp
+++ src/team4/calculator.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include<vector>
 #include<string.h>
+#include "UI.h"
 using namespace std;
 using namespace Utilities;
 
@@ -9,6 +10,7 @@ using namespace Utilities;
 /*this is example main class*/
 /*int main()
 {
+    
     Calculator *cal=new Calculator();
     cal->execute();
     return 0;
@@ -20,20 +22,32 @@ void Calculator::execute()
     stack.init();
     //strcpy(cal_printout,"the answer");
     char exp[100];
-    cout << "enter expression end such as (1+2)*(3-4)   :";
+    UI::println( "Enter expression such as the expression (1+2)*(3-4)   :");
     
-    cin >> exp;
+    string expression=UI::readLine();
+    strcpy(exp,expression.c_str());
     char post[100] ;
     int n =0;
     if(!postfix(exp,post,n))
     {
         char a[]= "answer: ";
         printA(a);
-        cout<<cal_printout;
-        cout<<postfix_value(post)<<endl;
+        //cout<<cal_printout;
+        string tmp=to_string(postfix_value(post));
+        char b[30];
+        strcpy(b,tmp.c_str());
+        
+        printA(b);
+        char c[]= "\n";
+        printA(c);
+        
+        UI::println( cal_printout);
+        
     }
     else
-        cout<<cal_printout<<endl;
+        UI::println( cal_printout);
+
+    
 }
 
 
@@ -45,7 +59,23 @@ void Calculator::execute()
  */
 bool Calculator::CheckSyntax(char pre[])
 {
-    if(checkCharacters(pre)||checkOP(pre)||checkDot(pre)|| checkBracket(pre)||checkSeicolon(pre))
+    if(checkCharacters(pre))
+    {
+        return 1;
+    }
+    else if(checkOP(pre))
+    {
+        return 1;
+    }
+    else if(checkBracket(pre))
+    {
+        return 1;
+    }
+    else if(checkDot(pre))
+    {
+        return 1;
+    }
+    else if(checkSeicolon(pre))
     {
         return 1;
     }
@@ -66,8 +96,8 @@ bool Calculator::checkCharacters(char pre[])
         {
             printError(pre,i);
             return 1;
+            
         }
-        
     }
     
     return 0;
@@ -81,7 +111,7 @@ bool Calculator::checkOP(char pre[])
     int i;
     /*
      operator cannot show in the first one or the last one;
-    */
+     */
     if(isoperator(pre[0])||isoperator(pre[(unsigned)strlen(pre)-1]))
     {
         char a[]="operator cannot be first or the end";
@@ -90,7 +120,7 @@ bool Calculator::checkOP(char pre[])
     }
     /*check for a set of operator
      such as "+++"
-    */
+     */
     for(i=0;i<(unsigned)strlen(pre);i++)
     {
         if(isoperator(pre[i]))
@@ -168,7 +198,7 @@ bool Calculator::checkBracket(char pre[])
         printError(a);
         return 1;
     }
-        if(((unsigned)strlen(pre))==(unsigned)strlen(b))
+    if(((unsigned)strlen(pre))==(unsigned)strlen(b))
     {
         char a[]="you need number in the expression as well";
         printError(a);
@@ -205,7 +235,7 @@ void Calculator::printError(char array[],int l)
     {
         if(i==l)
             printA(bracketA);
-            printC(array[i]);
+        printC(array[i]);
         if(i==l)
             printA(bracketB);
     }
@@ -214,7 +244,7 @@ void Calculator::printError(char array[],int l)
 }
 void Calculator::printError(char array[])
 {
-     printA(array);
+    printA(array);
 }
 /*
  add a char array into the cal_print
@@ -237,17 +267,17 @@ void Calculator::printA(char a[])
 void Calculator::printC(char a)
 {
     cal_printout[(unsigned)strlen(cal_printout)]=a;
-    cal_printout[(unsigned)strlen(cal_printout)]='\0';
+    cal_printout[(unsigned)strlen(cal_printout)+1]='\0';
 }
 
-bool Calculator::isoperator(char op)  
-{  
+bool Calculator::isoperator(char op)
+{
     return ((op=='+')||(op=='-')||(op=='*')||(op=='/'));
-}  
-  
-  
-int Calculator::priority(char op)  
-{  
+}
+
+
+int Calculator::priority(char op)
+{
     
     if(op==';')
     {
@@ -267,138 +297,139 @@ int Calculator::priority(char op)
     }
     return 1;
 }
-  
-  
-int Calculator::postfix(char pre[] ,char post[],int &n)  
-{ 
-   if(CheckSyntax(pre))
-	{
-		char a[]="\n    --- Syntax issuse ---   ";
-		printA(a);
-		return 1;
-	}
-    int i=0,j=0;
-   n=0;
+
+
+int Calculator::postfix(char pre[] ,char post[],int &n)
+{
+    memset( &cal_printout, 0, sizeof( char )*200 );
+    if(CheckSyntax(pre))
+    {
+        char a[]="\n    --- Syntax issuse ---   ";
+        printA(a);
+        return 1;
+    }
+    int i=0,j=0,q=0;
+    n=0;
     MyStack<char> stack;
-     
-    stack.init();        
-  
-    stack.push(';');    
-  
-  while(pre[i]!=';')  
-    {  
-        if((pre[i]>='0' && pre[i] <='9')||pre[i] =='.')  
-        {  
-            post[j++] = pre[i];  
-            n++;  
-        }  
-        else if (pre[i]=='(')    
-            stack.push(pre[i]);  
-        else if(pre[i] ==')')    
-        {  
-            while(stack.gettop()!='(')  
-            {  
-                post[j++] = stack.pop();  
-                n++;  
-            }  
-            stack.pop();   
-        }  
-        else if (isoperator(pre[i]))  
-        {  
-            post[j++] = ' ';   
-            n++;  
-            while(priority(pre[i]) <= priority(stack.gettop()))   
-            {  
-                 
-                post[j++] = stack.pop();  
-                n++;  
-            }  
-  
-            stack.push(pre[i]);   
-        }  
-  
-        i++;  
-    }  
-    while(stack.top)   
-    {  
-        post[j++] = stack.pop();  
-        n++;  
-    } 
-	return 0;
-}  
-  
-double Calculator::read_number(char str[],int *i)  
-{  
-    double x=0.0;  
-    int k = 0;  
-    while(str[*i] >='0' && str[*i]<='9')  
-    {  
-        x = x*10+(str[*i]-'0');  
-        (*i)++;  
-    }  
-  
-    if(str[*i]=='.')  
-    {  
-        (*i)++;  
-        while(str[*i] >= '0'&&str[*i] <='9')  
-        {  
-            x = x * 10 + (str[*i]-'0');  
-            (*i)++;  
-            k++;  
-        }  
-    }  
-    while(k!=0)  
-    {  
-        x /= 10.0;  
-        k--;  
-    }  
-  
-    return x;  
-}  
-  
-double Calculator::postfix_value(char post[])  
-{  
-    MyStack<double> stack;     
-    stack.init();  
-  
-    int i=0;  
-    double x1,x2;  
-  
-   while(post[i] !=';')
-    {  
-        if(post[i] >='0' && post[i] <='9')  
-            stack.push(read_number(post,&i));  
-        else if(post[i] == ' ')  
-            i++;  
-        else if (post[i] =='+')  
-        {  
-            x2 = stack.pop();  
-            x1 = stack.pop();  
-            stack.push(x1+x2);  
-            i++;  
-        }  
-        else if (post[i] =='-')  
-        {  
-            x2 = stack.pop();  
-            x1 = stack.pop();  
-            stack.push(x1-x2);  
-            i++;  
-        }  
-        else if (post[i] =='*')  
-        {  
-            x2 = stack.pop();  
-            x1 = stack.pop();  
-            stack.push(x1*x2);  
-            i++;  
-        }  
-        else if (post[i] =='/')  
-        {  
-            x2 = stack.pop();  
-            x1 = stack.pop();  
-            stack.push(x1/x2);  
-            i++;  
-        }  
-    }  
+    
+    stack.init();
+    
+    stack.push(';');
+    
+    while(pre[i]!=';')
+    {
+        if((pre[i]>='0' && pre[i] <='9')||pre[i] =='.')
+        {
+            post[j++] = pre[i];
+            n++;
+        }
+        else if (pre[i]=='(')
+            stack.push(pre[i]);
+        else if(pre[i] ==')')
+        {
+            while(stack.gettop()!='(')
+            {
+                post[j++] = stack.pop();
+                n++;
+            }
+            stack.pop();
+        }
+        else if (isoperator(pre[i]))
+        {
+            post[j++] = ' ';
+            n++;
+            while(priority(pre[i]) <= priority(stack.gettop()))
+            {
+                
+                post[j++] = stack.pop();
+                n++;
+            }
+            
+            stack.push(pre[i]);
+        }
+        
+        i++;
+    }
+    while(stack.top)
+    {
+        post[j++] = stack.pop();
+        n++;
+    }
+    return 0;
+}
+
+double Calculator::read_number(char str[],int *i)
+{
+    double x=0.0;
+    int k = 0;
+    while(str[*i] >='0' && str[*i]<='9')
+    {
+        x = x*10+(str[*i]-'0');
+        (*i)++;
+    }
+    
+    if(str[*i]=='.')
+    {
+        (*i)++;
+        while(str[*i] >= '0'&&str[*i] <='9')
+        {
+            x = x * 10 + (str[*i]-'0');
+            (*i)++;
+            k++;
+        }
+    }
+    while(k!=0)
+    {
+        x /= 10.0;
+        k--;
+    }
+    
+    return x;
+}
+
+double Calculator::postfix_value(char post[])
+{
+    MyStack<double> stack;
+    stack.init();
+    
+    int i=0,j ;
+    double x1,x2;
+    
+    while(post[i] !=';')
+    {
+        if(post[i] >='0' && post[i] <='9')
+            stack.push(read_number(post,&i));
+        else if(post[i] == ' ')
+            i++;
+        else if (post[i] =='+')
+        {
+            x2 = stack.pop();
+            x1 = stack.pop();
+            stack.push(x1+x2);
+            i++;
+        }
+        else if (post[i] =='-')
+        {
+            x2 = stack.pop();
+            x1 = stack.pop();
+            stack.push(x1-x2);
+            i++;
+        }
+        else if (post[i] =='*')
+        {
+            x2 = stack.pop();
+            x1 = stack.pop();
+            stack.push(x1*x2);
+            i++;
+        }
+        else if (post[i] =='/')
+        {
+            x2 = stack.pop();
+            x1 = stack.pop();
+            stack.push(x1/x2);
+            i++;
+        }
+    }
     return stack.gettop();  
 }  
 
